b113eda7826d6a2fc07a08de9870d1d25738658c
alekseiiagnenkov <alekseiiagnenkov@gmail.com>
2022-05-12 14:37:24 +0300

    test

--- src/main/java/ru/c19501/Application.java
+++ src/main/java/ru/c19501/Application.java
@@ -5,8 +5,8 @@ import ru.c19501.core.FileSystemFactory;
 import ru.c19501.core.system.FileSystemFactoryImpl;
 import ru.c19501.service.CoreService;
 import ru.c19501.service.CoreServiceImpl;
-import ru.c19501.service.model.FileRecordDTO;
 import ru.c19501.service.config.JacksonConfig;
+import ru.c19501.model.FileRecord.FileRecordReturnDTO;
 
 public class Application {
     public static void main(String[] args) {
@@ -25,10 +25,13 @@ public class Application {
             coreService.createFile("a3", "txt", 1);
             coreService.createFile("a4", "txt", 1);
 
-            for (FileRecordDTO file : coreService.readFiles()) {
+            for (FileRecordReturnDTO file : coreService.readFiles()) {
                 System.out.println(file);
             }
+
+            System.out.println(coreService.foundFile("a5", "txt"));
         }
+
         fileSystem.save();
     }
 }

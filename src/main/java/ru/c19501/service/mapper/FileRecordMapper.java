634eb48fbdf5fa3e87cf492c4b29e6cd43ea4cc0
alekseiiagnenkov <alekseiiagnenkov@gmail.com>
2022-05-12 14:37:54 +0300

    created Mapper FileRecordDTO to FileRecordReturnDTO

---
+++ src/main/java/ru/c19501/service/mapper/FileRecordMapper.java
@@ -0,0 +1,16 @@
+package ru.c19501.service.mapper;
+
+import ru.c19501.model.FileRecord.FileRecordDTO;
+import ru.c19501.model.FileRecord.FileRecordReturnDTO;
+
+public abstract class FileRecordMapper {
+
+    public static FileRecordReturnDTO dtoToReturnDto(FileRecordDTO dto){
+        return FileRecordReturnDTO.builder()
+                .fileName(dto.getFileName())
+                .fileType(dto.getFileType())
+                .creationDate(dto.getCreationDate())
+                .build();
+    }
+
+}

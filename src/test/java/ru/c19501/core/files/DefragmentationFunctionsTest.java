b2a0ca35a73608a4731b4d1efa27da30de1a1c20
dmitr <dmitriy.porter@mail.ru>
2022-05-16 10:20:36 +0300

    Tests

---
+++ src/test/java/ru/c19501/core/files/DefragmentationFunctionsTest.java
@@ -0,0 +1,29 @@
+package ru.c19501.core.files;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DefragmentationFunctionsTest {
+
+    @Test
+    void maxLengthToInsert() {
+
+    }
+
+    @Test
+    void possibleToInsert() {
+    }
+
+    @Test
+    void howMuchSpace() {
+    }
+
+    @Test
+    void defradExt() {
+    }
+
+    @Test
+    void checkDef() {
+    }
+}

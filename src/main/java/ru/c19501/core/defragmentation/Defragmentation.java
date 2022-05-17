94f3d2d258992bd789694dc383a282f9be8ffdc1
v.d.olennikov <v.d.olennikov@tcsbank.ru>
2022-05-17 15:51:19 +0300

    fix defragment bug

--- src/main/java/ru/c19501/core/defragmentation/Defragmentation.java
+++ src/main/java/ru/c19501/core/defragmentation/Defragmentation.java
@@ -8,18 +8,23 @@ import java.util.List;
 
 public class Defragmentation {
     public static void defragment(Segment segment) {
-        int currentIndex = 0;
-        int nextReplacedIndex = 0;
         List<FileRecord> fileRecords = segment.getFileRecords();
 
+        //find first deleted item
+        int currentIndex = 0;
+        while (!fileRecords.get(currentIndex).isDeleted()) {
+            currentIndex++;
+        }
+
         //move deleted items into the end
+        int nextReplacedIndex = currentIndex;
         while (currentIndex < fileRecords.size()) {
             if (!fileRecords.get(currentIndex).isDeleted() && currentIndex > nextReplacedIndex) {
                 Collections.swap(fileRecords, currentIndex, nextReplacedIndex);
-                nextReplacedIndex += 1;
+                nextReplacedIndex++;
             }
 
-            currentIndex += 1;
+            currentIndex++;
         }
 
         //numbering the elements of the array

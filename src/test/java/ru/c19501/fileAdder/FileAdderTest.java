f43ec4a3a1525cff4d90cfe2c9372854347f815e
SlaykSD <60920077+SlaykSD@users.noreply.github.com>
2022-05-26 23:08:49 +0300

    add new test for FileAdderTest

--- src/test/java/ru/c19501/fileAdder/FileAdderTest.java
+++ src/test/java/ru/c19501/fileAdder/FileAdderTest.java
@@ -59,8 +59,8 @@ public class FileAdderTest{
     void addFileRecordIntoDeletedSpace() {
         List<Segment> list = new ArrayList<>();
         ArrayList<FileRecord> listRec = new ArrayList<>();
-        listRec.add(getFileRecordNotDeleted(new FileRecord("a1","txt",0,2,0)));
-        listRec.add(getFileRecordDeleted(new FileRecord("a2","txt",2,2,1)));
+        listRec.add(getFileRecordNotDeleted(new FileRecord("a1","txt",0,1,0)));
+        listRec.add(getFileRecordDeleted(new FileRecord("a2","txt",1,3,1)));
         list.add(new Segment(2,0,listRec));
         listRec = new ArrayList<>();
         listRec.add(getFileRecordNotDeleted(new FileRecord("a3","txt",4,2,0)));
@@ -128,7 +128,7 @@ public class FileAdderTest{
         }
     }
 
-    //Number 3 Failed
+    //Number 3
     @Test
     void addFileRecordIntoDeletedAndFreeSpace() {
         List<Segment> list = new ArrayList<>();
@@ -151,15 +151,8 @@ public class FileAdderTest{
 
         JsonRepository  repository2 =  new JsonRepository(6,0,0,list);
 
-        try {
-            repository.addFileRecord("a4","txt",3);
-            System.out.println(StringUtils.difference(repository.getCurrentJson(),repository2.getCurrentJson()));
+        assertThrows(CoreException.class,() -> repository.addFileRecord("a4","txt",3));
 
-            System.out.println(repository.getCurrentJson());
-            assertEquals(repository.getCurrentJson(),repository2.getCurrentJson());
-        } catch (CoreException | JsonProcessingException e) {
-            e.printStackTrace();
-        }
     }
 
     //Number 4

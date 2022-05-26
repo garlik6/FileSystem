0d89e17ff7bf1368ed4fec8f231a8deda94cfbaf
SlaykSD <60920077+SlaykSD@users.noreply.github.com>
2022-05-26 22:42:28 +0300

    add new test for FileAdderTest

--- src/test/java/ru/c19501/fileAdder/FileAdderTest.java
+++ src/test/java/ru/c19501/fileAdder/FileAdderTest.java
@@ -29,15 +29,8 @@ public class FileAdderTest{
         fileRecord.setFileStatus(FileRecord.FileStatus.DELETED);
         return fileRecord;
     }
-    private FileSystemFactory fileSystemFactory = new FileSystemFactoryImpl();
-    private  FileSystem fileSystem = fileSystemFactory.getSystem();
 
-    @BeforeEach
-    public void refreshFileSystem(){
-        fileSystemFactory = new FileSystemFactoryImpl();
-        fileSystem = fileSystemFactory.getSystem();
-    }
-
+    //Number 0
     @Test
     void addFileRecordIntoFreeSpace() {
         List<Segment> list = new ArrayList<>();
@@ -61,6 +54,7 @@ public class FileAdderTest{
 
     }
 
+    //Number 1
     @Test
     void addFileRecordIntoDeletedSpace() {
         List<Segment> list = new ArrayList<>();
@@ -98,7 +92,7 @@ public class FileAdderTest{
     }
 
 
-    //Случай 2
+    //Number 2
     @Test
     void addFileRecordIntoDeletedSpace2() {
         List<Segment> list = new ArrayList<>();
@@ -134,33 +128,75 @@ public class FileAdderTest{
         }
     }
 
+    //Number 3 Failed
     @Test
-    void addFileRecordNeededDefragmentation() throws CoreException {
+    void addFileRecordIntoDeletedAndFreeSpace() {
         List<Segment> list = new ArrayList<>();
-        list.add(new Segment(2));
-        list.add(new Segment(2));
         ArrayList<FileRecord> listRec = new ArrayList<>();
-        listRec.add(getFileRecordNotDeleted(new FileRecord("a1","txt",0,1,0)));
-        listRec.add(getFileRecordDeleted(new FileRecord("a2","txt",1,3,1)));
+        listRec.add(getFileRecordNotDeleted(new FileRecord("a1","txt",0,3,0)));
+        listRec.add(getFileRecordDeleted(new FileRecord("a2","txt",3,2,1)));
         list.add(new Segment(2,0,listRec));
-        listRec = new ArrayList<FileRecord>();
-        listRec.add(getFileRecordNotDeleted(new FileRecord("a3","txt",4,2,0)));
-        list.add(new Segment(2,4,listRec));
+        listRec = new ArrayList<>();
+        list.add(new Segment(2,5,listRec));
 
-        JsonRepository repository =  new JsonRepository(6,0,3,list);
+        JsonRepository  repository =  new JsonRepository(6,1,3,list);
+
         list = new ArrayList<>();
         listRec = new ArrayList<>();
-        listRec.add(getFileRecordNotDeleted(new FileRecord("a1","txt",0,1,0)));
-        listRec.add(getFileRecordDeleted(new FileRecord("a4","txt",1,2,1)));
-        listRec.add(new FileRecord("","",3,1,2));
+        listRec.add(getFileRecordNotDeleted(new FileRecord("a1","txt",0,3,0)));
+        listRec.add(getFileRecordNotDeleted(new FileRecord("a4","txt",3,3,1)));
         list.add(new Segment(2,0,listRec));
         listRec = new ArrayList<>();
-        listRec.add(getFileRecordNotDeleted(new FileRecord("a3","txt",4,2,0)));
-        list.add(new Segment(2,4,listRec));
+        list.add(new Segment(2,6,listRec));
 
-        JsonRepository  repository2 =  new JsonRepository(6,0,1,list);
-        Throwable thrown = assertThrows(CoreException.class, () -> {
-            repository.addFileRecord("a4","txt",2);
-        });
+        JsonRepository  repository2 =  new JsonRepository(6,0,0,list);
+
+        try {
+            repository.addFileRecord("a4","txt",3);
+            System.out.println(StringUtils.difference(repository.getCurrentJson(),repository2.getCurrentJson()));
+
+            System.out.println(repository.getCurrentJson());
+            assertEquals(repository.getCurrentJson(),repository2.getCurrentJson());
+        } catch (CoreException | JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
+
+    //Number 4
+    @Test
+    void addFileRecordIntoDeletedAndFreeSpace2() {
+        List<Segment> list = new ArrayList<>();
+        ArrayList<FileRecord> listRec = new ArrayList<>();
+        listRec.add(getFileRecordNotDeleted(new FileRecord("a1","txt",0,3,0)));
+        listRec.add(getFileRecordDeleted(new FileRecord("a2","txt",3,2,1)));
+        listRec.add(new FileRecord("","",5,1,2));
+        list.add(new Segment(2,0,listRec));
+
+        listRec = new ArrayList<>();
+        list.add(new Segment(2,6,listRec));
+
+        JsonRepository  repository =  new JsonRepository(6,0,3,list);
+
+        list = new ArrayList<>();
+        listRec = new ArrayList<>();
+        listRec.add(getFileRecordNotDeleted(new FileRecord("a1","txt",0,3,0)));
+        listRec.add(getFileRecordNotDeleted(new FileRecord("a4","txt",3,3,1)));
+        list.add(new Segment(2,0,listRec));
+
+        listRec = new ArrayList<>();
+        list.add(new Segment(2,6,listRec));
+
+        JsonRepository  repository2 =  new JsonRepository(6,0,0,list);
+
+        try {
+            repository.addFileRecord("a4","txt",3);
+            System.out.println(StringUtils.difference(repository.getCurrentJson(),repository2.getCurrentJson()));
+
+            assertEquals(repository.getCurrentJson(),repository2.getCurrentJson());
+        } catch (CoreException | JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }

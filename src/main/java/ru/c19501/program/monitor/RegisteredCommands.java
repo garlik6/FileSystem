e19b7960acb13d561e5b215dd27ebd37b20eb90a
optim <optimustm@mail.ru>
2022-05-27 10:24:02 +0300

    2ю

--- src/main/java/ru/c19501/program/monitor/RegisteredCommands.java
+++ src/main/java/ru/c19501/program/monitor/RegisteredCommands.java
@@ -12,9 +12,8 @@ public class RegisteredCommands {
         registeredCommands.put("create", CreateFile.class.getName());
         registeredCommands.put("delete", DeleteFile.class.getName());
         registeredCommands.put("print", Print.class.getName());
-        registeredCommands.put("find", FindFile.class.getName());
         registeredCommands.put("help", Help.class.getName());
-        registeredCommands.put("start", Hello.class.getName());
+        registeredCommands.put("down", Hello.class.getName());
         registeredCommands.put("defrag", Defragmentation.class.getName());
     }
 

84373da63d70bd2cdfc9cde24dc0e560e7cfc902
optim <optimustm@mail.ru>
2022-05-27 20:26:08 +0300

    2юa

--- src/main/java/ru/c19501/program/monitor/RegisteredCommands.java
+++ src/main/java/ru/c19501/program/monitor/RegisteredCommands.java
@@ -12,7 +12,7 @@ public class RegisteredCommands {
         registeredCommands.put("create", CreateFile.class.getName());
         registeredCommands.put("delete", DeleteFile.class.getName());
         registeredCommands.put("print", Print.class.getName());
-        registeredCommands.put("print¿", PrintA.class.getName());
+        registeredCommands.put("printA", PrintA.class.getName());
         registeredCommands.put("help", Help.class.getName());
         registeredCommands.put("commands", AllComm.class.getName());
         registeredCommands.put("download", Hello.class.getName());

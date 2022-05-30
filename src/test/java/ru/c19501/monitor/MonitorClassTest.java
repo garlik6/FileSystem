fab1b67ecb390017b2bfd9db215420fd279bbe72
optim <optimustm@mail.ru>
2022-05-30 16:50:02 +0300

    monitor test

---
+++ src/test/java/ru/c19501/monitor/MonitorClassTest.java
@@ -0,0 +1,75 @@
+package ru.c19501.monitor;
+
+import ru.c19501.program.function.*;
+import ru.c19501.program.monitor.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import ru.c19501.system.*;
+import java.lang.reflect.InvocationTargetException;
+
+
+
+public class MonitorClassTest {
+    static MonitorClass monitor = new MonitorClass(new FileSystemFactoryImpl().getSystem(), new StreamActionsFake());
+    @Test
+    /**
+     * Проверяем функцию выдачи класса функции системы на правильную команду
+     */
+    public void checkRunFunction0() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        RegisteredCommands.init(); //инициализируем список всех команд
+        var actual = monitor.runFunction("resize"); //запустили нашу функцию
+        assertTrue(actual instanceof Resize); //проверили что достался нужный клас
+    }
+    @Test
+    public void checkRunFunction1() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        RegisteredCommands.init(); //инициализируем список всех команд
+        var actual = monitor.runFunction("create"); //запустили нашу функцию
+        assertTrue(actual instanceof CreateFile); //проверили что достался нужный клас
+    }
+    @Test
+    public void checkRunFunction3() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        RegisteredCommands.init(); //инициализируем список всех команд
+        var actual = monitor.runFunction("defrag"); //запустили нашу функцию
+        assertTrue(actual instanceof Defragmentation); //проверили что достался нужный клас
+    }
+    @Test
+    public void checkRunFunction4() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        RegisteredCommands.init(); //инициализируем список всех команд
+        var actual = monitor.runFunction("delete"); //запустили нашу функцию
+        assertTrue(actual instanceof DeleteFile); //проверили что достался нужный клас
+    }
+    @Test
+    public void checkRunFunction6() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        RegisteredCommands.init(); //инициализируем список всех команд
+        var actual = monitor.runFunction("print"); //запустили нашу функцию
+        assertTrue(actual instanceof Print); //проверили что достался нужный клас
+    }
+    @Test
+    public void checkRunFunction7() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        RegisteredCommands.init(); //инициализируем список всех команд
+        var actual = monitor.runFunction("printA"); //запустили нашу функцию
+        assertTrue(actual instanceof PrintA); //проверили что достался нужный клас
+    }
+    @Test
+    public void checkRunFunction8() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        RegisteredCommands.init(); //инициализируем список всех команд
+        var actual = monitor.runFunction("help"); //запустили нашу функцию
+        assertTrue(actual instanceof Help); //проверили что достался нужный клас
+    }
+    @Test
+    public void checkRunFunction9() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        RegisteredCommands.init(); //инициализируем список всех команд
+        var actual = monitor.runFunction("info"); //запустили нашу функцию
+        assertTrue(actual instanceof Info); //проверили что достался нужный клас
+    }
+    /**
+     * Проверяем функцию выдачи класса функции системы на некорректную команду
+     */
+    @Test
+    public void checkRunFunction10() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        RegisteredCommands.init(); //инициализируем список всех команд
+        String  commandName = "exe"; //некоректная команда
+        var actual = monitor.runFunction(commandName); //запустили нашу функцию
+        assertNull(actual); //проверили что null
+    }
+}
---
+++ src/test/java/ru/c19501/monitor/StreamActionsFake.java
@@ -0,0 +1,34 @@
+package ru.c19501.monitor;
+
+import ru.c19501.program.struct.iStreamActions;
+
+import java.util.ArrayList;
+
+public class StreamActionsFake implements iStreamActions {
+    ArrayList<String> stringListInput = new ArrayList<String>();
+    ArrayList<String> stringListOutput = new ArrayList<String>();
+
+    public void println(String value) {
+        print(value);
+        stringListOutput.add("");
+
+    }
+    public void print(String value) {
+        if (stringListOutput.isEmpty()) stringListOutput.add("");
+
+        stringListOutput.add(stringListOutput.remove(stringListOutput.size() - 1) + value);
+
+
+    }
+    public String getLine() {
+        if (stringListInput.isEmpty())
+            return "бширх";
+        stringListOutput.add("");
+        return stringListInput.remove(0);
+    }
+    public int nextInt() {
+        if (stringListInput.isEmpty())
+            return 0;
+        return Integer.parseInt(stringListInput.remove(0));
+    }
+}
